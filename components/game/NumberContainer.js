7bd1c09215031b47319fa8e191a888560152ee09
efsofian <ef.sofian@gmail.com>
2022-07-22 09:23:16 +0200

    modif padding

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
 		borderWidth: 4,
 		borderColor: Colors.accent500,
 		margin: deviceWidth < 380 ? 12 : 24,
-		margin: deviceWidth < 380 ? 12 : 24,
+		padding: deviceWidth < 380 ? 12 : 24,
 		borderRadius: 8,
 		alignItems: "center",
 		justifyContent: "center",
